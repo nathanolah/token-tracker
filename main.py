942c66b876566c882bcaa4820940bd81659a5ffa
Nathan Olah <nolah@myseneca.ca>
2024-02-29 20:50:54 -0500

    added db

--- main.py
+++ main.py
@@ -4,20 +4,40 @@ import requests
 
 from observer import observer
 from singleton import DatabaseConnection
-
+from utils.db_helpers import DBHelpers
 
 print("\n")
 print("Token-Tracker")
 print("********************")
 
-#Login/Signup
-#
 db = DatabaseConnection()
 conn = db.connect()
 mycursor = conn.cursor()
 
+db_helpers = DBHelpers()
+
+# Login/Sign-up
 
 
+
+
+# Insert User
+# db_helpers.insert_user("john123", "doe123")
+
+# Insert Token
+# db_helpers.insert_token("0x6982508145454Ce325dDbE47a25d4ec3d2311933")
+
+# Insert token for a user
+# db_helpers.insert_token_for_user(7,1)
+
+# Retrive tokens for a user by username
+# tokens = db_helpers.retrive_tokens_for_user('user123')
+# print('Tokens for user:')
+# for token in tokens:
+#     print(token[0])
+
+# Remove token for a user
+# db_helpers.add_token_for_user('user123', '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2')
 
 
 db.close()
---
+++ utils/db_helpers.py
@@ -0,0 +1,131 @@
+#
+#
+import mysql.connector
+from mysql.connector import errorcode
+from singleton import DatabaseConnection
+
+db = DatabaseConnection()
+conn = db.connect()
+cursor = conn.cursor()
+
+class DBHelpers:
+    # Insert new user into db
+    def insert_user(self, username, password):
+        sql = 'INSERT INTO users (username, password) VALUES (%s, %s)'
+        val = (username, password)
+        try:
+            cursor.execute(sql, val)
+            conn.commit()
+            print('User inserted successfully.')
+            
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_DUP_ENTRY:
+                print(f"Error: User '{username}' already exists.")
+            else:
+                print(f'Error: {err}')
+
+    # Insert new token by token address into db 
+    def insert_token(self, token_address):
+        sql = 'INSERT INTO tokens (token_address) VALUES (%s)'
+        try:
+            cursor.execute(sql, (token_address,))
+            conn.commit()
+            print('Token inserted successfully.')
+
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_DUP_ENTRY:
+                print(f"Error: Token '{token_address}' already exists")
+            else:
+                print(f'Error: {err}')
+
+    # Associate a user with a token (add to user's portfolio)
+    def insert_token_for_user(self, user_id, token_id):
+        sql = 'INSERT INTO user_tokens (user_id, token_id) VALUES (%s, %s)'
+        try:
+            cursor.execute(sql, (user_id, token_id))
+            conn.commit()
+            print('Token associated with user successfully.')
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_DUP_ENTRY:
+                print(f"Error: user id: {user_id} is already associated with token id: {token_id}")
+            else:
+                print(f'Error: {err}') 
+
+    # Retrive users tokens in portfolio
+    def retrive_tokens_for_user(self, username):
+        sql = '''
+                SELECT tokens.token_address 
+                FROM users
+                JOIN user_tokens ON users.user_id = user_tokens.user_id
+                JOIN tokens ON user_tokens.token_id = tokens.token_id
+                WHERE users.username = %s
+            '''
+        try:
+            cursor.execute(sql, (username,))
+            tokens = cursor.fetchall()
+            if not tokens:
+                print(f"User: '{username}' not found")
+
+            return tokens
+        except mysql.connector.Error as err:
+            print(f'Error: {err}')
+
+    # Add token to user's portfolio
+    def add_token_for_user(self, username, token_address):
+        try:
+            token_id = None
+            sql = 'SELECT token_id FROM tokens WHERE token_address = %s'
+            cursor.execute(sql, (token_address,))
+            result = cursor.fetchone()
+
+            if result:
+                token_id = result[0]
+                # print("Token already exists. Token ID:", token_id)
+            else:
+                # token not found, insert token
+                sql = 'INSERT INTO tokens (token_address) VALUES (%s)'
+                cursor.execute(sql, (token_address,))
+                conn.commit()
+                token_id = cursor.lastrowid
+                # print("Token inserted. Token ID:", token_id)
+
+            sql = 'SELECT user_id FROM users WHERE username = %s'
+            cursor.execute(sql, (username,))
+            user_id = cursor.fetchone()[0]
+
+            self.insert_token_for_user(user_id, token_id)
+        
+        except mysql.connector.Error as err:
+            print(f'Error: {err}')
+
+    # 
+    # Database Setup/Reset
+    #
+    def reset_database(self):
+        cmd = input('Are you sure you want to reset the database y/n?')
+        if (cmd == 'y'):
+            cursor.execute('DROP TABLE IF EXISTS users')
+            cursor.execute('DROP TABLE IF EXISTS tokens')
+            cursor.execute('DROP TABLE IF EXISTS user_tokens')
+
+            sql = '''CREATE TABLE users (
+                        user_id INT AUTO_INCREMENT PRIMARY KEY, 
+                        username VARCHAR(255) UNIQUE, 
+                        password VARCHAR(255)
+                    )'''
+            cursor.execute(sql)
+
+            sql = '''CREATE TABLE tokens (
+                        token_id INT AUTO_INCREMENT PRIMARY KEY, 
+                        token_address VARCHAR(255) UNIQUE
+                    )'''
+            cursor.execute(sql)
+
+            sql = '''CREATE TABLE user_tokens (
+                        user_id INT,
+                        token_id INT, 
+                        FOREIGN KEY (user_id) REFERENCES users(user_id), 
+                        FOREIGN KEY (token_id) REFERENCES tokens(token_id),
+                        PRIMARY KEY (user_id, token_id)     
+                    )'''
+            cursor.execute(sql)
